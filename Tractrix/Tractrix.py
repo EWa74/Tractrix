435cabdcf6263314ef92081a5417d9abb12bac54
EWa74 <eric.wahl@web.de>
2014-08-12 07:34:29 +0200

    draft....

--- Tractrix/Tractrix.py
+++ Tractrix/Tractrix.py
@@ -98,9 +98,8 @@ class createMatrix(object):
 
 
   
-class Tractrix_OT_Main (bpy.types.Operator, ImportHelper): # OT fuer Operator Type
-    writelog('Tractrix_OT_Main- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ') 
-    writelog('- - - - - - - - - - - - -  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ')
+class Tractrix_OT_Main (bpy.types.Operator): # OT fuer Operator Type
+   
     ''' Import selected curve '''
     bl_idname = "object.tractrix"
     bl_label = "Tractrix_OT_Main (TB)" #Toolbar - Label
@@ -109,22 +108,140 @@ class Tractrix_OT_Main (bpy.types.Operator, ImportHelper): # OT fuer Operator Ty
     #                                  parameters of this operator interactively 
     #                                  (in the Tools pane) 
 
-    # ImportHelper mixin class uses this
-    filename_ext = ".dat"
-
-    filter_glob = StringProperty(
-            default="*.dat",
-            options={'HIDDEN'},
-            )
- 
+    
     def execute(self, context):  
-        writelog('- - - - - - - - - - - - -  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ')
-        writelog(' FUNKTIONSAUFRUF Tractrix_OT_Main')
-        writelog('- - - - - - - - - - - - -  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ')
+        
+        i= 0
+        # 1. Schleife von start_frame to end_frame
+        int_fStart = bpy.data.scenes['Scene'].frame_start
+        int_fStop = bpy.data.scenes['Scene'].frame_end
+        
+        int_Count = int_fStop-int_fStart
+        
+        for i in range(0,int_Count,1):
+            print("int_fcurrent: " + str(i))
+            bpy.data.scenes['Scene'].frame_current= i
+            
+        
+        #bpy.data.scenes['Scene'].frame_current
+        
+        
+        # 1.1 Position von Traktor in Abh.keit vom aktuellen frame
+        # 1.2 Traxtrix-Funktion fuettern
+        # 1.3-a entweder Keyframe für jedes Frame des Trailers setzen
+        # 1.3-b nur die Nurbspath Koordinaten nutzen und eine Trailer-Nurbs auf Basis der 
+        # Traktor-Nurbs errechnen
        
+       
+       
+        #Traktor = bpy.data.objects['Obj1']      
+        #action_name = bpy.data.objects[Traktor.name].animation_data.action.name
+        
+        #action=bpy.data.actions[action_name]
+        #locID, rotID = FindFCurveID(Traktor, action)
+        #action.fcurves[locID[0]].keyframe_points[0].co      
+        
+        #bpy.data.curves[objCurve.name].splines
+        
+        #get_absolute()
         return {'FINISHED'} 
-    writelog('Tractrix_OT_Main done')
+    writelog('- - Tractrix_OT_Main done- - - - - - -')
        
+
+   
+
+
+def get_absolute(Obj_Koord, Obj_Angle, BASEPos_Koord, BASEPos_Angle):
+    # Obj_Koord und Obj_Angle sind lokale Angaben bezogen auf Base
+    # Aufruf bei Import
+    # Obj_Koord, Obj_Angle [rad]: relativ
+    # BASEPos_Koord, BASEPos_Angle [rad]: absolut 
+    
+    # Transformation Local2World
+    
+    # Gegeben: Mtrans, Mrot = Base --> Mworld/ Mrot_rel, Vtrans_rel --> Mworld_rel
+    # Ges:  Vtrans_abs, Mrot_abs
+    #
+    # Mworld  / Mworld_rel / Mworld_abs mit world = trans * rot
+    # Mtrans  / Mtrans_rel / Mtrans_abs
+    # Mrot    / Mrot_rel   / Mrot_abs
+    
+    # 01012014 objBase = bpy.data.objects['Sphere_BASEPos']
+    #bpy.data.objects[Obj.name].rotation_mode =RotationModeTransform
+    
+    matrix_world =bpy.data.objects[objBase.name].matrix_world
+    #point_local  = Obj_Koord 
+    
+    Mtrans     = mathutils.Matrix.Translation(Vector(BASEPos_Koord))
+    Vtrans_rel = Obj_Koord                              #lokal 
+    writelog('Vtrans_rel'+ str(Vtrans_rel))  # neuer Bezugspunkt
+      
+    MrotX = mathutils.Matrix.Rotation(BASEPos_Angle[0], 3, 'X') # C = -179 Global
+    MrotY = mathutils.Matrix.Rotation(BASEPos_Angle[1], 3, 'Y') # B = -20
+    MrotZ = mathutils.Matrix.Rotation(BASEPos_Angle[2], 3, 'Z') # A = -35
+    Mrot = MrotZ * MrotY * MrotX
+    writelog('Mrot'+ str(Mrot))
+    
+    Mworld = Mtrans * Mrot.to_4x4()
+    
+    Mrot_relX = mathutils.Matrix.Rotation(Obj_Angle[0], 3, 'X') # Local (bez. auf Base)
+    Mrot_relY = mathutils.Matrix.Rotation(Obj_Angle[1], 3, 'Y') # 0,20,35 = X = -C, Y = -B, Z = -A
+    Mrot_relZ = mathutils.Matrix.Rotation(Obj_Angle[2], 3, 'Z')
+    Mrot_rel = Mrot_relZ * Mrot_relY * Mrot_relX # KUKA Erg.
+    writelog('Mrot_rel'+ str(Mrot_rel))
+
+    Mrot_abs = Mrot_rel.transposed() * Mrot.transposed()       
+    Mrot_abs = Mrot_abs.transposed()
+    rotEuler =Mrot_abs.to_euler('XYZ')
+    
+    writelog('rotEuler'+ str(rotEuler))
+    writelog('rotEuler[0] :'+ str(rotEuler[0]*360/(2*math.pi)))
+    writelog('rotEuler[1] :'+ str(rotEuler[1]*360/(2*math.pi)))
+    writelog('rotEuler[2] :'+ str(rotEuler[2]*360/(2*math.pi)))
+        
+    Vtrans_abs = Mworld *Vtrans_rel
+    writelog('Vtrans_abs :'+ str(Vtrans_abs))
+       
+    return Vtrans_abs, rotEuler
+
+def FindFCurveID(objEmpty_A6, action):
+    writelog('_____________________________________________________________________________')
+    writelog('FindFCurveID')
+   
+    #ob_target = objEmpty_A6
+    # todo: Unklar: mehrere Actions moeglich?! -> fuehrt ggf. zu einer Liste als Rueckgabewert:
+    
+    writelog(action.name)
+    
+    locID  =[9999, 9999, 9999]
+    rotID  =[9999, 9999, 9999, 9999]
+    scaleID=[9999, 9999, 9999]
+    dlocID =[9999, 9999, 9999]
+         
+    action_data =action.fcurves
+    writelog(action_data)
+    
+    for v,action_data in enumerate(action_data):
+        if action_data.data_path == "location":
+            locID[action_data.array_index] = v
+            #ob_target.delta_location[action_data.array_index]=v
+            writelog("location[" + str(action_data.array_index) + "] to (" + str(v) + ").")
+        elif action_data.data_path == "rotation_quaternion":
+            rotID[action_data.array_index] = v
+            #ob_target.delta_rotation_euler[action_data.array_index]=v
+            writelog("rotation_quaternion[" + str(action_data.array_index) + "] to (" + str(v) + ").")
+        elif action_data.data_path == "scale":
+            scaleID[action_data.array_index] = v
+            #ob_target.delta_scale[action_data.array_index]=v
+            writelog("scale[" + str(action_data.array_index) + "] to (" + str(v) + ").")
+        elif action_data.data_path == "delta_location":
+            dlocID[action_data.array_index] = v
+            #ob_target.delta_scale[action_data.array_index]=v
+            writelog("delta_location[" + str(action_data.array_index) + "] to (" + str(v) + ").")
+        else:
+            writelog("Unsupported data_path [" + action_data.data_path + "].")
+    
+      
 class Tractrx_PT_Panel(bpy.types.Panel):
     writelog('_____________________________________________________________________________')
     writelog()
