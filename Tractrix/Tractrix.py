ceab3fa0ab3448b853741c6dc60686c548683ea6
EWa74 <eric.wahl@web.de>
2015-03-16 07:43:16 +0100

    commit and push test 2015 from dell

--- Tractrix/Tractrix.py
+++ Tractrix/Tractrix.py
@@ -1,4 +1,4 @@
-
+# delete this line (commit and push test 2015 from dell)
 #  ***** BEGIN GPL LICENSE BLOCK *****ewa 
 #  https://github.com/EWa74/KUKA_Simulator.git
 #  This program is free software: you can redistribute it and/or modify
